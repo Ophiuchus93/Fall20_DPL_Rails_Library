999551143385dcc854270ae0130f100f6161e2e6
Scott (DC)  Christensen <schristensen10@gmail.com>
2020-08-24 18:59:12 -0600

    Full Crud Works

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,53 @@
+class BooksController < ApplicationController
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+    
+  end
+
+  def new
+    @book = Book.new
+
+    render :_form
+  end
+
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      redirect_to books_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    render :_form
+  end
+  
+  def update
+    if @book.update(book_params)
+      redirect_to @book
+    else
+      render :edit
+    end
+  end
+
+def destroy
+  @book.destroy
+    redirect_to books_path
+end
+
+  private
+    def set_book
+      @book = Book.find(params[:id])
+    end
+
+    def book_params
+      params.require(:book).permit(:title, :author, :pages)
+    end
+end
